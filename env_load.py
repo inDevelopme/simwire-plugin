83e280f53f679ed0ef8b5b8304ad4ff2fba75628
dkbaldwin <dkbaldwin@indevelopme.com>
2023-12-20 06:06:31 -0600

    #42 Part 1 - The code to load the environments is not required to be a part of the main part of the flask application
    created single file that can be used to load flask environment files. It is completely unaware of how other flask / related components.

---
+++ env_load.py
@@ -0,0 +1,49 @@
+import os
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+# it is critical that instance variables stay in a segment of code
+# that never needs to change such as the current instance's environment name
+# load the business access object for communicating with the database
+
+class Config(object):
+    DEBUG = True
+    TESTING = False
+    SESSION_TYPE = 'sqlalchemy'
+    SQLALCHEMY_ECHO = True
+    PERMANENT_SESSION_LIFETIME = 86400
+    MYSQL_CONFIGURATION = {
+        'connection_type': '',
+        'username': '',
+        'password': '',
+        'hostname': '',
+        'database': ''
+    }
+    SQLALCHEMY_DATABASE_URI = ''
+    SECRET_KEY = ''
+    SESSION_SQLALCHEMY = None
+
+    def set_server_side_session(self, db):
+        self.SESSION_SQLALCHEMY = db
+
+    def get_environment_config(self):
+        environment_filepaths = [
+            join(dirname(__file__), '.env.local'),
+            join(dirname(__file__), '.env.aws')
+        ]
+
+        for environment_file in environment_filepaths:
+            # setup reader for environment file
+            dotenv_path = join(environment_file)
+            load_dotenv(dotenv_path)
+
+        ENV_NAME = os.environ.get("ENV_NAME")
+        if (ENV_NAME is None) or (ENV_NAME == "local"):
+            MYSQL_USER = os.environ.get("MYSQL_USER")
+            MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+            MYSQL_DB = os.environ.get("MYSQL_DB")
+            MYSQL_HOST = os.environ.get("MYSQL_HOST")
+            MYSQL_CONNECTOR = os.environ.get("MYSQL_CONNECTOR")
+            self.SQLALCHEMY_DATABASE_URI = \
+                MYSQL_CONNECTOR + '://' + MYSQL_USER + ':' + MYSQL_PASSWORD + '@' + MYSQL_HOST + '/' + MYSQL_DB
+            self.SECRET_KEY = 'your secret key'
